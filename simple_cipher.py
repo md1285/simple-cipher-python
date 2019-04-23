694cbcbb882830cf86f9453718970efb01fbe8c2
Mike Dwyer <brianmichaeldwyer@gmail.com>
2019-04-23 14:54:32 -0700

    create working class

---
+++ simple_cipher.py
@@ -0,0 +1,42 @@
+class Cipher():
+  def encode(self, string, shift = 1):
+    try:
+      # redefine arguments
+      while shift > 26:
+        shift = shift - 26
+      alphabet = list('abcdefghijklmnopqrstuvwxyz')
+      def find_letter_idx(letter):
+        return alphabet.index(letter)
+      string = list(map(find_letter_idx, list(string.lower())))
+      # perform shift operations
+      res = []
+      for num in string:
+        if num + shift < 25:
+          res.append(alphabet[num + shift])
+        else: 
+          res.append(alphabet[((num + shift) % 25) - 1])
+      # return result
+      return ''.join(res)
+    except (err):
+      raise Exception('An error occurred: ' + err)
+
+  def decode(self, string, shift = 1):
+    try:
+      # redefine arguments
+      while shift > 26:
+        shift = shift - 26
+      alphabet = list('abcdefghijklmnopqrstuvwxyz')
+      def find_letter_idx(letter):
+        return alphabet.index(letter)
+      string = list(map(find_letter_idx, list(string.lower())))
+      # perform shift operations
+      res = []
+      for num in string:
+        if num - shift < 25:
+          res.append(alphabet[num - shift])
+        else: 
+          res.append(alphabet[((num - shift) % 25) - 1])
+      # return result
+      return ''.join(res)
+    except (err):
+      raise Exception('An error occurred: ' + err)
--- simple_cipher_test.py
+++ simple_cipher_test.py
@@ -12,105 +12,105 @@ class SimpleCipherTest(unittest.TestCase):
         ciphertext = 'b'
         self.assertEqual(ciphertext, cipher.encode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_letter_with_default_shift_distance(self):
         cipher = Cipher()
         plaintext = 'b'
         ciphertext = 'a'
         self.assertEqual(ciphertext, cipher.decode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_wraps_z_to_a(self):
         cipher = Cipher()
         plaintext = 'z'
         ciphertext = 'a'
         self.assertEqual(ciphertext, cipher.encode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_wraps_a_to_z(self):
         cipher = Cipher()
         plaintext = 'a'
         ciphertext = 'z'
         self.assertEqual(ciphertext, cipher.decode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_string_with_default_shift_distance(self):
         cipher = Cipher()
         plaintext = 'wxyzabcd'
         ciphertext = 'xyzabcde'
         self.assertEqual(ciphertext, cipher.encode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_string_with_default_shift_distance(self):
         cipher = Cipher()
         plaintext = 'xyzabcde'
         ciphertext = 'wxyzabcd'
         self.assertEqual(ciphertext, cipher.decode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_with_10_shift_distance(self):
         cipher = Cipher()
         plaintext = 'wxyzabcd'
         ciphertext = 'ghijklmn'
         self.assertEqual(ciphertext, cipher.encode(plaintext, 10))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_with_10_shift_distance(self):
         cipher = Cipher()
         plaintext = 'ghijklmn'
         ciphertext = 'wxyzabcd'
         self.assertEqual(ciphertext, cipher.decode(plaintext, 10))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_with_26_shift_distance(self):
         cipher = Cipher()
         plaintext = 'abcdefghij'
         ciphertext = 'abcdefghij'
         self.assertEqual(ciphertext, cipher.encode(plaintext, 26))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_with_26_shift_distance(self):
         cipher = Cipher()
         plaintext = 'klmnopqrst'
         ciphertext = 'klmnopqrst'
         self.assertEqual(ciphertext, cipher.decode(plaintext, 26))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_with_27_shift_distance(self):
         cipher = Cipher()
         plaintext = 'wxyzabcd'
         ciphertext = 'xyzabcde'
         self.assertEqual(ciphertext, cipher.encode(plaintext, 27))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_with_27_shift_distance(self):
         cipher = Cipher()
         plaintext = 'xyzabcde'
         ciphertext = 'wxyzabcd'
         self.assertEqual(ciphertext, cipher.decode(plaintext, 27))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_always_returns_downcase_string(self):
         cipher = Cipher()
         plaintext = 'AbCdEfGhIj'
         ciphertext = 'bcdefghijk'
         self.assertEqual(ciphertext, cipher.encode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_always_returns_downcase_string(self):
         cipher = Cipher()
         plaintext = 'BcDeFgHiJk'
         ciphertext = 'abcdefghij'
         self.assertEqual(ciphertext, cipher.decode(plaintext))
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_encode_raise_error_if_non_letter_characters(self):
         cipher = Cipher()
         plaintext = 'az4'
         with pytest.raises(Exception):
             cipher.encode(plaintext)
 
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_decode_raise_error_if_non_letter_characters(self):
         cipher = Cipher()
         plaintext = 'az4'
